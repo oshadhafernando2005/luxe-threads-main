78690cb8864345c6f5236a599f755a6eefee3328
oshadhafernando2005 <oshadhafernando1234@gmail.com>
2026-09-07 14:55:25 +0530

    change fav icon

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -155,7 +155,7 @@ export const products: Product[] = [
   {
     id: "noir-oversized",
     name: "Summer Cropped T-Shirt",
-    price: 2500,
+    price: 25,
     gender: "women",
     category: "Crop",
     colors: [ "White", "Blue","Pink"],
